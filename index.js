6bd1692921abad1006fd6e9b630724bc497ff4c0
leansoda99 <86275758+leansoda99@users.noreply.github.com>
2023-05-01 21:56:18 -0300

    proyecto finalizado

--- index.js
+++ index.js
@@ -9,8 +9,27 @@ fetch("./data.json")
     let memory=data[1];
     let verbal=data[2];
     let visual=data[3];
+    document.getElementById("category1").innerHTML = reaction.category;
+    document.getElementById("score1").innerHTML = reaction.score;
+    const icon1 =document.getElementById("icon1");
+    icon1.src= reaction.icon;
+
+    document.getElementById("category2").innerHTML = memory.category;
+    document.getElementById("score2").innerHTML = memory.score;
+    const icon2 =document.getElementById("icon2");
+    icon2.src= memory.icon;
+
+    document.getElementById("category3").innerHTML = verbal.category;
+    document.getElementById("score3").innerHTML = verbal.score;
+    const icon3 =document.getElementById("icon3");
+    icon3.src= verbal.icon;
+
+    document.getElementById("category4").innerHTML = visual.category;
+    document.getElementById("score4").innerHTML = visual.score;
+    const icon4 =document.getElementById("icon4");
+    icon4.src= visual.icon;
   })
   .catch(error => {
     alert('Error al cargar el archivo JSON');
   });
-  document.getElementById("reaction").innerHTML = "hola"
+  
